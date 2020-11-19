d60fd5c5a696207e79c587069156b571253fbe55
linmingdao <mingzhaod@LinMingdaodeMac-mini.local>
2020-11-19 17:08:08 +0800

    目录结构调整

---
+++ src/App copy.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+// import logo from "./logo.svg";
+import "antd/dist/antd.css";
+import "./App.css";
+// import LikeButton from "./components/LikeButton";
+// import Hello from "./components/Hello";
+// import CounterWrapper from "./components/CounterWrapper";
+// import BizDemoPercent from "./components/BizDemoPercent";
+// import BizDemoAverage from "./components/BizDemoAverage";
+// import BizDemoDoubleY from "./components/BizDemoDoubleY";
+// import useURLLoader from "./hooks/useURLLoader";
+// import ReactDndDemo from "./ReactDndDemo";
+// import ReactSortableDemo from "./ReactSortableDemo";
+// import ReactSortableHocDemo from "./ReactSortableHocDemo/index.jsx";
+// import ReactDndSortableDemo from "./ReactDndSortableDemo";
+// import H5EditorDemo from "./H5EditorDemo";
+import EditorV2 from "./H5Editor";
+
+interface IShowResult {
+  message: string;
+  status: string;
+}
+
+interface IThemeProps {
+  [key: string]: { color: string; background: string };
+}
+
+const themes: IThemeProps = {
+  light: {
+    color: "#000",
+    background: "#eee",
+  },
+  dark: {
+    color: "#fff",
+    background: "#222",
+  },
+};
+
+export const ThemeContext = React.createContext(themes.light);
+
+const App: React.FC = () => {
+  const [show, setShow] = useState(true);
+  return (
+    <div className="App">
+      <ThemeContext.Provider value={themes.dark}>
+        <header className="App-header">
+          <EditorV2 />
+          {/* <H5EditorDemo />
+          <ReactDndSortableDemo />
+          <ReactSortableHocDemo />
+          <ReactSortableDemo />
+          <ReactDndDemo />
+          <BizDemoDoubleY />
+          <BizDemoPercent />
+          <BizDemoAverage />
+          <CounterWrapper />
+          <img src={logo} className="App-logo" alt="logo" />
+          <LikeButton />
+          <Hello />
+          <p>
+            <button
+              onClick={() => {
+                setShow(!show);
+              }}
+            >
+              Refresh dog photo
+            </button>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+        </header>
+      </ThemeContext.Provider>
+    </div>
+  );
+};
+
+export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import React, { useState } from "react";
-// import logo from "./logo.svg";
+import React from "react";
 import "antd/dist/antd.css";
 import "./App.css";
-// import LikeButton from "./components/LikeButton";
-// import Hello from "./components/Hello";
-// import CounterWrapper from "./components/CounterWrapper";
-// import BizDemoPercent from "./components/BizDemoPercent";
-// import BizDemoAverage from "./components/BizDemoAverage";
-// import BizDemoDoubleY from "./components/BizDemoDoubleY";
-// import useURLLoader from "./hooks/useURLLoader";
-// import ReactDndDemo from "./ReactDndDemo";
-// import ReactSortableDemo from "./ReactSortableDemo";
-// import ReactSortableHocDemo from "./ReactSortableHocDemo/index.jsx";
-// import ReactDndSortableDemo from "./ReactDndSortableDemo";
-// import H5EditorDemo from "./H5EditorDemo";
-import EditorV2 from "./EditorV2";
-
-interface IShowResult {
-  message: string;
-  status: string;
-}
+import H5Editor from "./H5Editor";
 
 interface IThemeProps {
   [key: string]: { color: string; background: string };
@@ -39,41 +21,11 @@ const themes: IThemeProps = {
 export const ThemeContext = React.createContext(themes.light);
 
 const App: React.FC = () => {
-  const [show, setShow] = useState(true);
   return (
     <div className="App">
       <ThemeContext.Provider value={themes.dark}>
         <header className="App-header">
-          <EditorV2 />
-          {/* <H5EditorDemo />
-          <ReactDndSortableDemo />
-          <ReactSortableHocDemo />
-          <ReactSortableDemo />
-          <ReactDndDemo />
-          <BizDemoDoubleY />
-          <BizDemoPercent />
-          <BizDemoAverage />
-          <CounterWrapper />
-          <img src={logo} className="App-logo" alt="logo" />
-          <LikeButton />
-          <Hello />
-          <p>
-            <button
-              onClick={() => {
-                setShow(!show);
-              }}
-            >
-              Refresh dog photo
-            </button>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
+          <H5Editor />
         </header>
       </ThemeContext.Provider>
     </div>
